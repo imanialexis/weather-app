ca7f59a31fa9f637ed49f0a61a447c13b4a8c36f
imani alexis <alexis.bailey37@gmail.com>
2018-11-26 15:35:23 -0500

    data populates for weather component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ import Weather from "./components/Weather";
 const API_KEY = process.env.REACT_APP_WEATHER_API;
 
 class App extends Component {
-    // state {
-
-    // }
+    state = {
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined 
+    }
     getWeather = async (e) => {
       e.preventDefault();
       const city = e.target.elements.city.value;
@@ -20,15 +25,32 @@ class App extends Component {
       `);
       //takes api_call and converts it to json
       const data = await api_call.json();
-      console.log('this is the data => ',data)
+      // console.log('this is the data => ',data)
+      // console.log('this should be the temp',Math.floor(data.main.temp))
+      this.setState({
+        temperature : Math.floor(data.main.temp),
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: undefined 
 
+      })
     }
   render() {
     return (
       <div> 
         <Titles /> 
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+          
+          />
       </div>
     );
   }
