f0b715ed912219993c1f84a0b460b25cfbc65be1
imani alexis <alexis.bailey37@gmail.com>
2018-11-26 13:58:28 -0500

    changed units to imperial because, america

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = process.env.REACT_APP_WEATHER_API
+const API_KEY = process.env.REACT_APP_WEATHER_API;
 
 class App extends Component {
+    // state {
+
+    // }
+    getWeather = async (e) => {
+      e.preventDefault();
+      const city = e.target.elements.city.value;
+      const country = e.target.elements.country.value;
+      console.log('this is the city =>',city)
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}
+      &APPID=${API_KEY}&units=imperial
+      `);
+      //takes api_call and converts it to json
+      const data = await api_call.json();
+      console.log('this is the data => ',data)
+
+    }
   render() {
     return (
-      <div>
+      <div> 
         <Titles /> 
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
